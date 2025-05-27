c1fdb453e5afca41d985f445541f40e638adef86
pinis <pinishlomi@gmail.com>
2025-05-27 16:25:51 +0300

    add manage plots starter

--- src/starters/manage_plots/models/Villa.java
+++ src/starters/manage_plots/models/Villa.java
@@ -1,6 +1,6 @@
 package starters.manage_plots.models;
 
 public class Villa {
-    private boolean hasRoof; // Assuming "גג" implies a specific feature, perhaps for solar panels etc.
+    private boolean hasRoof;
     private boolean hasPrivateParking;
 }
